eebaeeba5417f742b814c4b5bcf5823cc794253f
Erfyan <erfyantaubat@gmail.com>
2025-12-23 18:02:30 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga fugit odit perferendis ipsa. Quas corrupti, ullam nesciunt beatae vitae eaque, id ipsa nobis labore, temporibus enim sequi repellendus amet alias.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
